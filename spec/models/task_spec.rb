b667717c286958088aac680bf7a68ea107295ba2
tungchinlee <tungchinli@gmail.com>
2020-10-31 22:52:01 +0800

    task spec done

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  
+  it "task belongs to user" do
+    user = User.create(name: "u1")
+    task = Task.create(title: "t1", user_id: user.id)
+    expect(user.tasks).to include(task) 
+  end
 end
